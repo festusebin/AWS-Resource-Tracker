7df94c28acc5eed677477a16c422f2959bed4172
festusebin <ebinfestus@gmail.com>
2024-04-04 19:20:08 +0100

    added an output file in the shell script

--- aws_resource_tracker.sh
+++ aws_resource_tracker.sh
@@ -25,17 +25,17 @@ set -x # debug mode
 
 # list of s3 buckets
 echo "Print list of s3 buckets"
-aws s3 ls
+aws s3 ls >> resourceTracker
 
 # list of EC2 Instances
 echo "Print list of EC2 Instances"
-aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId' >> resourceTracker
 
 # list of lambda functions
 echo "Print list of lambda functions"
-aws lambda list-functions
+aws lambda list-functions >> resourceTracker
 
 # list of IAM Users
 echo "Print list of IAM Users"
-aws iam list-users 
+aws iam list-users >> resourceTracker
 
